ca4cc3c3d074264f66d29b726622a2bbce2a8b3d
Mark Dewey <mark.t.dewey@gmail.com>
2017-08-14 14:59:50 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IntroToInheriantance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
